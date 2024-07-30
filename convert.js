c9743e18d10da41b2b26d98b7d09b3f1496e2adb
04utkarsh-k <UtkarshKaurav04@gmail.com>
2024-07-30 13:35:00 +0530

    Added convert.js

---
+++ convert.js
@@ -0,0 +1,58 @@
+const baseURL="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";//from=EUR&to=USD
+const dropdowns=document.querySelectorAll(".dropdown select");
+let btn=document.querySelector("#btn");
+const fromCurr=document.querySelector(".From select");
+const toCurr=document.querySelector(".To select");
+const msg=document.querySelector(".msg");
+
+for(let select of dropdowns){
+    for(currCode in countryList){
+        let newOption=document.createElement("option");
+        newOption.innerText=currCode;
+        newOption.value=currCode;
+        if(select.name==="From" && currCode==="USD"){
+           newOption.selected="selected";
+        }else if(select.name==="To" && currCode==="INR"){
+            newOption.selected="selected";
+         }
+        select.append(newOption);
+    }
+    select.addEventListener("change",(evt)=>{
+        updateFlag(evt.target);
+    });
+}
+
+const updateExchangeRate= async ()=>{
+    let amount=document.querySelector(".amount input");
+    let amtVal=amount.value;
+    if(amtVal==="" || amtVal<1){
+        amtVal=1;
+        amount.value="1";
+    }
+    //console.log(fromCurr.value,toCurr.value);
+    const URL=`${baseURL}/${fromCurr.value.toLowerCase()}.json`;
+    let response=await fetch(URL);
+    let data=await response.json();
+    //console.log(data);
+    let rate=data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    console.log(rate);
+    let finalAmount=amtVal*rate;
+    msg.innerText=`${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+}
+
+const updateFlag=(element)=>{
+    let currCode=element.value;
+    let countryCode=countryList[currCode];
+    newSrc=`https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img=element.parentElement.querySelector("img");
+    img.src=newSrc;
+}
+
+btn.addEventListener("click",async (evt)=>{
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+window.addEventListener("load",()=>{
+    updateExchangeRate();
+});
